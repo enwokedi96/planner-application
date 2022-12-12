268cbfabeca3dfc25f9f7b0896b5308e646fc46b
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-12 03:32:14 +0000

    time put in first columns

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,21 @@
 $( 'document' ).ready(function(){
+    var timePeriod;
     var currentDate = $('#currentDay')
     var timeblocks = $('.container')
 
     currentDate.text(moment().format('LL'))
 
     for (var i=0; i<9; i++){
-        var newRow = $("<div></div>").attr({"class":"row","id":`${i}`})
-        
+        var newRow = $("<div></div>").attr({"class":"row",
+                                            "id":`${i}`,
+                                            });
+        ((9+i)<12)? timePeriod = 'am': timePeriod = 'pm';
         for (var j=0; j<3; j++){
             var newCol = $(`<div class='col' id =${j}>one</div>`)
             newRow.append(newCol)
             if (j===0){
-
+                newCol.attr("class","justify-items-")
+                newCol.text(9+i+':00'+timePeriod)
             }
 
         }
