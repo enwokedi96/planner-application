3049c833b4f41b9215e40f119401f052111034e9
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-12 22:30:40 +0000

    added icon and new column css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,38 @@
 $( 'document' ).ready(function(){
     var time;
+    var extTime;
     var timePeriod;
     var currentDate = $('#currentDay')
     var timeblocks = $('.container')
-
+    var rowClasses = ['col-sm-2 col-md-3 col-lg-2',
+                        'col-sm-8 col-md-8 col-lg-8',
+                        'col-sm-2 col-md-1 col-lg-2']
+    var currentHour = moment().hours();
     currentDate.text(moment().format('LL'))
 
     for (var i=0; i<9; i++){
-        var newRow = $("<div></div>")
-        newRow.attr({"class":"row","id":`${i}`,});
-        //newRow.css("background-color", "yellow")
+        var newRow = $("<div class='row'></div>")
         
         for (var j=0; j<3; j++){
-            var newCol = $(`<div class='col' id =${j}>one</div>`)
+            var newCol = $(`<div class=${rowClasses[j]} id =${j}></div>`)
             newRow.append(newCol)
             if (j===0){
-                newCol.attr({"class":"justify-content-start",
-                            "class":"align-items-middle"})
+                newCol.attr("class","align-items-start",)
+                newCol.css("background-color","#deff6fff")
                 time = 9+i;
                 (time<12)? timePeriod = 'am': timePeriod = 'pm';
-                (time<10)?time='0'+`${time}`:time=`${time}`
-                newCol.text(time+':00'+timePeriod)
-                newCol.css({"border-radius": "10px",})
+                (time<10)?extTime='0'+`${time}`:extTime=`${time}`
+                newCol.text(extTime+':00'+timePeriod)
+                //newCol.css({"border-radius": "10px", })
             }
-            newCol.css({"border": "solid 2px #76adff",})
+            else if (j===2){
+                //newCol.attr("class","justify-content-center",)
+                newCol.prepend(`<i class="fas fa-save">`)
+            }
+            newCol.css({"display":"flexbox",
+                        "border": "solid 2px #76adff",
+                        "border-radius": "10px",
+                        "padding-top":"25px"})
 
         }
         timeblocks.append(newRow)
