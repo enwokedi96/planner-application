e2f79996224dcf721222cb3324b4039cd794a504
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-12 23:01:49 +0000

    changed custom css to file css

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,9 +4,9 @@ $( 'document' ).ready(function(){
     var timePeriod;
     var currentDate = $('#currentDay')
     var timeblocks = $('.container')
-    var rowClasses = ['col-sm-2 col-md-3 col-lg-2',
-                        'col-sm-8 col-md-8 col-lg-8',
-                        'col-sm-2 col-md-1 col-lg-2']
+    var rowClasses = ['col-sm-2 col-md-2 col-lg-1',
+                        'col-sm-7 col-md-8 col-lg-10',
+                        'col-sm-3 col-md-2 col-lg-2']
     var currentHour = moment().hours();
     currentDate.text(moment().format('LL'))
 
@@ -17,22 +17,20 @@ $( 'document' ).ready(function(){
             var newCol = $(`<div class=${rowClasses[j]} id =${j}></div>`)
             newRow.append(newCol)
             if (j===0){
-                newCol.attr("class","align-items-start",)
-                newCol.css("background-color","#deff6fff")
+                newCol.css("background-color","gold")
                 time = 9+i;
                 (time<12)? timePeriod = 'am': timePeriod = 'pm';
                 (time<10)?extTime='0'+`${time}`:extTime=`${time}`
                 newCol.text(extTime+':00'+timePeriod)
-                //newCol.css({"border-radius": "10px", })
+                newCol.addClass("time-block")
             }
             else if (j===2){
-                //newCol.attr("class","justify-content-center",)
+                newCol.attr("class","saveBtn",)
                 newCol.prepend(`<i class="fas fa-save">`)
             }
-            newCol.css({"display":"flexbox",
-                        "border": "solid 2px #76adff",
-                        "border-radius": "10px",
-                        "padding-top":"25px"})
+            newRow.css({"display":"flexbox",
+                        //"border": "solid 2px #76adff",
+                        "margin-top":"10px"})
 
         }
         timeblocks.append(newRow)
