93dac3d64fc8b5f92992f3d35f9318dc6f1efe62
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-13 00:29:43 +0000

    added comments for each column

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,25 +11,27 @@ $( 'document' ).ready(function(){
     currentDate.text(moment().format('LL'))
 
     for (var i=0; i<9; i++){
-        var newRow = $("<div class='row time-block rowStyle'></div>")
+        var newRow = $("<div class='row rowStyle'></div>")
         
         for (var j=0; j<3; j++){
             var newCol = $(`<div class=${rowClasses[j]} id ='${j}'></div>`)
-            
+            // time blocks
             if (j===0){
                 newCol.css("background-color","gold")
                 time = 9+i;
                 (time<12)? timePeriod = 'am': timePeriod = 'pm';
                 (time<10)?extTime='0'+`${time}`:extTime=`${time}`
                 newCol.text(extTime+':00'+timePeriod)
-                newCol.addClass("hour")
+                newCol.addClass("hour time-block")
             }
+            // schedule blocks
             else if (j===1){
                 console.log(time,currentHour);
                 (time<currentHour)?newCol.addClass("past") :
                 (time==currentHour)?newCol.addClass("present") :
                 newCol.addClass("future"); 
             }
+            // save blocks
             else {
                 newCol.addClass("saveBtn")
                 newCol.prepend(`<i class="fas fa-save">`)
